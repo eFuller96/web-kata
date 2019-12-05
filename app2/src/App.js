1ef40025875998d3d80f58051af1cf73b28d80b5
Ellie <eleanor.fuller@red-gate.com>
2019-12-05 17:44:14 +0000

    Add form to add new products

--- app2/src/App.js
+++ app2/src/App.js
@@ -7,16 +7,56 @@ import Products from './Products.js'
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {props: data.products}
+    this.state = {products: data.products}
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmitProduct = this.handleSubmitProduct.bind(this)
   }
+
+  handleInputChange(event) {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmitProduct(event) {
+    event.preventDefault()
+
+    var updatedProductsArray = this.state.products.concat({
+      name: this.state.productName,
+      description: this.state.description
+    });
+    
+    this.setState({
+      products: updatedProductsArray })
+  }
+
   render() {
+
     return <div className="App">
       <div className="App-header">
         <h2>Kata 2- Add and remove objects</h2>
       </div>
-      <div className='add-product'>View to add product here...</div>
+      <div className='add-product'>
+        <form onSubmit={this.handleSubmitProduct}>
+          <label>Product Name
+            <input
+              name='productName'
+              onChange={this.handleInputChange} />
+          </label>
+          <label>Product Description
+            <input
+              name='description'
+              onChange={this.handleInputChange} />
+          </label>
+          <input type='submit' value='Submit'></input>
+        </form>
+      </div>
       <div className='products-container'>
-        <Products products={this.state.props} />
+        <Products products={this.state.products} />
       </div>
     </div>
   }
