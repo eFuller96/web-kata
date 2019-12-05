9abc0f654d3c65078a6dd5d46196ed784fdedf94
Ellie <eleanor.fuller@red-gate.com>
2019-12-05 14:22:27 +0000

    Extract products into separate component

--- app1/src/App.js
+++ app1/src/App.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react'
 import './App.css'
 import data from './data.js' 
 
-class App extends Component {
+class Product extends React.Component {
   render() {
     var products = data.products.map(x => 
-      <li key={x.name}>{x.name}</li>)
+        <li key={x.name}>{x.name}</li>)
+        return <div>{products}</div>
+  }
+}
 
+class App extends Component {
+  render() {
     return <div className="App">
       <div className="App-header">
         <h2>Welcome Introduction to <code>web-kata#1</code></h2>
       </div>
       <div className='products'>
-        {products}
+        <Product/>
       </div>
     </div>
   }
